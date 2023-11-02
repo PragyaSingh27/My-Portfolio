f4a81822264a3a30664adf78cc7823c89764de95
Pragya Singh <psingh272001@gmail.com>
2023-11-02 09:45:22 +0530

    New Changes

--- src/App.js
+++ src/App.js
@@ -1,6 +1,4 @@
-// https://github.com/bobangajicsm/react-portfolio-website/blob/master/src/components/Contact/index.scss
-
-import {Routes,Route} from 'react-router-dom';
+import {Routes,Route,BrowserRouter} from 'react-router-dom';
 import Layout from './components/Layout';
 import './App.scss';
 import Home from './components/Home';
@@ -10,17 +8,15 @@ import Contact from './components/Contact';
 function App() {
   return (
     <>
-
-    <Routes basename="/My-Portfolio">
-     
-      <Route path="/" element={<Layout />} >
-        <Route index element={<Home />} />
-        <Route path="about" element={<About />} />
-        <Route path="contact" element={<Contact />} />
-
-
-      </Route>
-    </Routes>
+    <BrowserRouter basename="/My-Portfolio">
+      <Routes >
+        <Route exact path="/" element={<Layout />} >
+          <Route index element={<Home />} />
+          <Route path="about" element={<About />} />
+          <Route path="contact" element={<Contact />} />
+        </Route>
+      </Routes>
+    </BrowserRouter>
     </>
   );
 }
